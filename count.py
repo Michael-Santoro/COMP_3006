e82a2a76089496fff1d95b8f9faa309edd14a762
Michael-Santoro <74157573+Michael-Santoro@users.noreply.github.com>
2021-01-24 20:50:32 -0700

    Getting Started on Assignment

---
+++ count.py
@@ -0,0 +1,29 @@
+#Michael Santoro
+#DU ID: 871869018
+#Description: Count characters and Case
+import sys
+
+def add_frequencies(d, file, remove_case):
+    '''Function takes in three parameters: a dictionary d, a file object file, and a boolean remove_case.
+    The dictionary maps characters to frequencies. Your function should add the frequency counts
+    of the characters in the file file to the dictionary d.
+    If remove_case is false, then the each character is the key into the dictionary. If remove_case
+    is true, then the lowercase of each character is the key into the dictionary.'''
+
+
+def main():
+    d = {}
+    if '-c' in sys.argv:
+
+    elif '-l' in sys.argv:
+
+    else:
+
+
+
+
+
+
+
+
+main()
