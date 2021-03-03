c263ef1816e19e3312f276e216aea1b4ab4068e8
Michael-Santoro <74157573+Michael-Santoro@users.noreply.github.com>
2021-03-02 21:16:07 -0700

    Finished initial code

--- diceGame.py
+++ diceGame.py
@@ -5,15 +5,15 @@
 import random
 
 #Define a Class
+#Dice Class
 class dice:
     def __init__(self,sides):
         self.sides = sides
         self.value = self.roll()
-        self.money = 1500
 
     def __str__(self):
-        s = f'You have: ${self.money} Your Roll was: {self.value}'
-        return self.money
+        s = f'Your Roll was: {self.value}'
+        return s
 
     #Equals
     def __eq__(self, other):
@@ -49,21 +49,107 @@ class dice:
 
     #Greater Than Equal
     def __ge__(self, other):
-        if self.value >= other.value:
+        if type(self) == type(other):
+            if self.value >= other.value:
+                return True
+            else:
+                NotImplemented
+
+    def __add__(self, other):
+        if type(self) == type(other):
+            self.value = self.value + other.value
+        else:
+            NotImplemented
+
+    def roll(self):
+        self.value = random.randint(1,self.sides)
+        print(f'{self} roll was {self.value}')
+        return self.value
+
+#A "Cup of Dice" class
+class cupOfDice:
+    def __init__(self, diceQty, sides):
+        self.cup = []
+        self.lastRoll = []
+        self.sum = 0
+        for i in range(diceQty):
+            self.cup.append(dice(sides))
+
+    def __str__(self):
+        s = f'{self} sum was: {self.value}'
+        return s
+
+    #Equals
+    def __eq__(self, other):
+        if self.sum == other.sum:
+            return True
+        else:
+            NotImplemented
+    #Not Equal
+    def __ne__(self, other):
+        if self.sum != other.sum:
+            return True
+        else:
+            NotImplemented
+    #Less Than
+    def __lt__(self, other):
+        if self.sum < other.sum:
+            return True
+        else:
+            NotImplemented
+    #Greater Than
+    def __gt__(self, other):
+        if self.sum > other.sum:
             return True
         else:
             NotImplemented
 
-    def __add__(self, other):
-        if self.value == other.value:
-            self.money = self.money
-        
-    def roll(self):
-        self.value = random.randint(1,self.sides)
-        return self.value
+    #Less Than Equal
+    def __le__(self, other):
+        if self.sum <= other.sum:
+            return True
+        else:
+            NotImplemented
+
+    #Greater Than Equal
+    def __ge__(self, other):
+        if self.sum >= other.sum:
+            return True
+        else:
+            NotImplemented
+
+    def cupRoll(self):
+        self.lastRoll = []
+        for die in self.cup:
+            r = die.roll()
+            self.sum += r
+            self.lastRoll.append(r)
+        return self.sum
+
+class player:
+    def __init__(self,qtyOfDice,sidesOnDie):
+        self.money = 1500
+        self.cup = cupOfDice(qtyOfDice,sidesOnDie)
+    def __str__(self):
+        return self.money
+
+def main():
+    qtyDice = input(f'Welcome To Dice War Game/nEnter the number of '
+                    f'dice you would like to play with')
+    numSides = input(f'Enter the number of sides you would like the die to have?')
+    user = player(qtyDice,numSides)
+    computer = player(qtyDice,numSides)
+    while user.money >0:
+        wager = input(f'You have: ${user} to wager. How much would you like to wager?')
+        if user.cup.cupRoll() > computer.cup.cupRoll():
+            user.money += wager
+        else:
+            user.money -= wager
+        print(f'Your sum was: {user.cup.sum} and the computers sum was: {computer.cup.sum}')
 
 
 
 
 
+main()
 
